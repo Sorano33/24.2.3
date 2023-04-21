a1321a4cc4dac24f70036d0e6ff0896c2448c5ab
Sorano33 <119518195+Sorano33@users.noreply.github.com>
2023-04-21 15:12:28 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+class Calculator:
+   def multiply(self, x, y):
+       return x * y
+
+   def division(self, x, y):
+       return x / yy
+
+   def subtractionsubtraction(self, x, y):
+       return x - y
+
+   def adding(self, x, y):
+       return x + y
